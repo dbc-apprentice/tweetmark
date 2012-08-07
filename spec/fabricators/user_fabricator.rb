1ffe7498aeda7d6c63f29215757ff30001eda5a5
June Jung <apprentice@devbootcamp.com>
2012-08-07 12:57:09 -0700

    Fabricator now working!!!!!!!

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,8 +1,9 @@
 Fabricator(:user) do
-  provider     "MyString"
-  uid          "MyString"
-  twitter_name "MyString"
-  name         "MyString"
-  auth_token   "MyString"
-  auth_secret  "MyString"
+  provider     "twitter"
+  uid           { (1..8).collect { rand(10) }.join }
+  twitter_name  { Faker::Name.first_name }
+  name          { Faker::Name.name }
+  auth_token    { (0...20).map{97.+(rand(25)).chr}.join }
+  auth_secret   { (0...30).map{65.+(rand(25)).chr}.join }
+  image_url     { Faker::Internet.domain_name }
 end
